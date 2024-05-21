430f935b1f320e510180f6a26c46f107cb2da2e5
PipoPlus <pipoplusnft@gmail.com>
2024-05-20 23:54:35 -0300

    Continuando el punto 3

---
+++ src/main/java/ejercicio3/ExportardorReporte.java
@@ -0,0 +1,7 @@
+package ejercicio3;
+
+import java.io.File;
+
+public interface ExportardorReporte {
+    void export(File file);
+}
---
+++ src/main/java/ejercicio3/Report.java
@@ -0,0 +1,33 @@
+package ejercicio3;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+class Report implements ExportardorReporte {
+    private String reporte;
+
+    public Report(String reporte) {
+        this.reporte = reporte;
+    }
+
+    @Override
+    public void export(File file) {
+        if (file == null) {
+            throw new IllegalArgumentException(
+                    "File es NULL; no puedo exportar..."
+            );
+        }
+        if (file.exists()) {
+            throw new IllegalArgumentException(
+                    "El archivo ya existe..."
+            );
+        }
+        // Exportar el reporte a un archivo.
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write(reporte);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
