4d7c4a3c7fede6993b300a40187a8d6b3f452f2e
PipoPlus <pipoplusnft@gmail.com>
2024-05-21 15:04:55 -0300

    Continuando el punto 4

--- src/main/java/ejercicio4/Main.java
+++ src/main/java/ejercicio4/Main.java
@@ -3,18 +3,23 @@ package ejercicio4;
 public class Main {
 
     public static void main(String[]args){
+        //Combo Basico
+        Combo comboBasico = new ComboBasico(100,"1 Hamburguesa");
 
-        Combo comboSimple = new ComboBasico(100,"1 Hamburguesa");
-        System.out.println(comboSimple.costoFinal());
-        System.out.println(comboSimple.darDescripcion());
+        System.out.println(comboBasico.costoFinal());
+        System.out.println(comboBasico.darDescripcion());
 
+        //Combo Familiar
         System.out.println("");
-        Combo comboFamiliar = new ComboFamiliar(comboSimple);
+        Combo comboFamiliar = new ComboFamiliar(comboBasico);
+
         System.out.println(comboFamiliar.costoFinal());
         System.out.println(comboFamiliar.darDescripcion());
 
+        //Combo Especial
         System.out.println("");
-        Combo comboEspecial = new ComboEspecial(comboSimple);
+        Combo comboEspecial = new ComboEspecial(comboBasico);
+
         System.out.println(comboEspecial.costoFinal());
         System.out.println(comboEspecial.darDescripcion());
 
